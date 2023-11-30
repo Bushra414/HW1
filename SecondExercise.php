f3ac1479a987f496751ccec8339c32f55693ecaa
Bushra414 <126082932+Bushra414@users.noreply.github.com>
2023-12-01 00:05:17 +0200

    HW1

--- SecondExercise.php
+++ SecondExercise.php
@@ -24,6 +24,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['textField'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: auto;
+            text-align: center;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
     <title>Document</title>
 </head>
 <body>
